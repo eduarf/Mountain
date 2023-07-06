2e7aa549dd5b78c99bd38f1a5243964d329692b9
eduarf <fraude_f@yahoo.com>
2023-07-06 03:52:27 +0300

    service and call-to-action section

--- data.js
+++ data.js
@@ -18,3 +18,17 @@ export const navItems = [
         name: 'Contact',
     },
 ];
+export const servicePhotos = [
+    {
+        id: nanoid(),
+        photoUrl: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+    },
+    {
+        id: nanoid(),
+        photoUrl: 'https://images.unsplash.com/photo-1454496522488-7a8e488e8606?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1476&q=80',
+    },
+    {
+        id: nanoid(),
+        photoUrl: 'https://images.unsplash.com/photo-1547093349-65cdba98369a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+    }
+];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,61 @@
-import './Home.scss';
+import "./Home.scss";
+import { servicePhotos } from "../../../data";
+import Button from "../../components/button/Button";
 const Home = () => {
-  return <div>
-    
-  </div>;
+  return (
+    <div>
+      {/* Service Section */}
+      <div className="service-container">
+        {servicePhotos.map((item, index) => {
+          return (
+            <div className="service" key={item.id}>
+              <img
+                src={item.photoUrl}
+                alt="mountain"
+                className="service__img"
+              />
+              <h2 className="service__header">Service {index + 1}</h2>
+              <div className="service__filter"></div>
+            </div>
+          );
+        })}
+      </div>
+      {/*  */}
+      {/* For You Section */}
+      <div className="for-you">
+        <div className="for-you__left">
+          <h1>Amazing Things for You</h1>
+        </div>
+        <div className="for-you__right">
+          <p>
+            Always write benefits over features. Mention all the benefits or
+            services with some real examples. Focus on how you can help and
+            benefit your user. Use simple words so that you don’t confuse
+            people. Say as much in as few words as possible.
+          </p>
+          <p>Speak directly to users using you not I or we.</p>
+          <Button buttonType="quinary" name="our history" />
+        </div>
+      </div>
+
+      {/* Call To Action */}
+      <div className="call-sec bg center">
+        <h1>Call To Action</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+          tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+        </p>
+        <Button name='contact' buttonType='tertiary' />
+      </div>
+      {/*  */}
+
+      {/* Photo albume */}
+      <div className="album-container">
+        
+      </div>
+      {/*  */}
+    </div>
+  );
 };
 
 export default Home;
