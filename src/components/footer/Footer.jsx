29af7e576421f4dbb749f7934b29066f31b6edec
eduarf <fraude_f@yahoo.com>
2023-07-06 19:15:38 +0300

    footer-styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,47 @@
 import "./Footer.scss";
-
+import { AiFillFacebook, AiFillMail, AiFillTwitterSquare } from 'react-icons/ai';
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer__top">
+        {/* Adress */}
+        <div className="footer__adress">
+          <div className="svg-icon"></div>
+          <h2>1600 Pennsylvania Ave Nw, Washington, Dc 20500</h2>
+          <div className="icon-container">
+              <AiFillFacebook className="icon" />
+              <AiFillMail className="icon" />
+              <AiFillTwitterSquare className="icon" />
+          </div>
+        </div>
+
+        {/* About Us */}
+        <div className="footer__about-us">
+          <h2>About Us</h2>
+          <ul>
+            <li>Company Profile</li>
+            <li>CSR Initiatives</li>
+            <li>Read Articles</li>
+            <li>Media Kit</li>
+          </ul>
+        </div>
+
+        {/* Connect */}
+        <div className="footer__connect">
+          <h2>Connect</h2>
+          <ul>
+            <li>Employee Portal</li>
+            <li>Customer Portal</li>
+            <li>Offices</li>
+            <li>Know More</li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer__copyright">
+        <p>Copyright © 2023 Mountain | Powered by Mountain</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
