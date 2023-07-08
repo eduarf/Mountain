dafdca1fd9173dec1734cb47bc3f98b58daa7cd9
eduarf <fraude_f@yahoo.com>
2023-07-08 20:07:35 +0300

    services page photos

--- data.js
+++ data.js
@@ -1,62 +1,88 @@
 import { nanoid } from "nanoid";
 
 export const navItems = [
-    {
-        id: nanoid(),
-        name: 'Home',
-    },
-    {
-        id: nanoid(),
-        name: 'About'
-    },
-    {
-        id: nanoid(),
-        name: 'Services',
-    },
-    {
-        id: nanoid(),
-        name: 'Contact',
-    },
+  {
+    id: nanoid(),
+    name: "Home",
+  },
+  {
+    id: nanoid(),
+    name: "About",
+  },
+  {
+    id: nanoid(),
+    name: "Services",
+  },
+  {
+    id: nanoid(),
+    name: "Contact",
+  },
 ];
 export const servicePhotos = [
-    {
-        id: nanoid(),
-        photoUrl: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
-    },
-    {
-        id: nanoid(),
-        photoUrl: 'https://images.unsplash.com/photo-1454496522488-7a8e488e8606?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1476&q=80',
-    },
-    {
-        id: nanoid(),
-        photoUrl: 'https://images.unsplash.com/photo-1547093349-65cdba98369a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
-    }
+  {
+    id: nanoid(),
+    photoUrl:
+      "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+  },
+  {
+    id: nanoid(),
+    photoUrl:
+      "https://images.unsplash.com/photo-1454496522488-7a8e488e8606?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1476&q=80",
+  },
+  {
+    id: nanoid(),
+    photoUrl:
+      "https://images.unsplash.com/photo-1547093349-65cdba98369a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+  },
+];
+
+export const servicesItems = [
+  {
+    id: nanoid(),
+    header: "Service 1",
+    description:
+      "A short description of your service. Lorem ipsm dolor amet, consec tetur adipis cing elit, sed do eiusmod tempor incid idunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exer citation ullamco la enim lorem isae ni.",
+    url: 'https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+  },
+  {
+    id: nanoid(),
+    header: "Service 2",
+    description:
+      "A short description of your service. Lorem ipsm dolor amet, consec tetur adipis cing elit, sed do eiusmod tempor incid idunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exer citation ullamco la enim lorem isae ni.",
+    url: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+  },
+  {
+    id: nanoid(),
+    header: "Service 3",
+    description:
+      "A short description of your service. Lorem ipsm dolor amet, consec tetur adipis cing elit, sed do eiusmod tempor incid idunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exer citation ullamco la enim lorem isae ni.",
+    url: 'https://plus.unsplash.com/premium_photo-1673457424939-d64bce7984e0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1543&q=80',
+  },
 ];
 
 export const photoAlbum = [
-    {
-        id: nanoid(),
-        url: 'https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
-    },
-    {
-        id: nanoid(),
-        url: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
-    },
-    {
-        id: nanoid(),
-        url: 'https://images.unsplash.com/photo-1502085671122-2d218cd434e6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1526&q=80',
-    },
-    {
-        id: nanoid(),
-        url: 'https://plus.unsplash.com/premium_photo-1673457424939-d64bce7984e0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1543&q=80',
-    },
-    {
-        id: nanoid(),
-        url: 'https://plus.unsplash.com/premium_photo-1675629119773-ba2f82432e11?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1493&q=80',
-    },
-    {
-        id: nanoid(),
-        url: 'https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1517&q=80',
-    },
-
+  {
+    id: nanoid(),
+    url: "https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+  },
+  {
+    id: nanoid(),
+    url: "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+  },
+  {
+    id: nanoid(),
+    url: "https://images.unsplash.com/photo-1502085671122-2d218cd434e6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1526&q=80",
+  },
+  {
+    id: nanoid(),
+    url: "https://plus.unsplash.com/premium_photo-1673457424939-d64bce7984e0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1543&q=80",
+  },
+  {
+    id: nanoid(),
+    url: "https://plus.unsplash.com/premium_photo-1675629119773-ba2f82432e11?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1493&q=80",
+  },
+  {
+    id: nanoid(),
+    url: "https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1517&q=80",
+  },
 ];
--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -1,7 +1,27 @@
-import './Services.scss';
+import "./Services.scss";
+import { servicesItems } from "../../../data";
+import Button from '../../components/button/Button';
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="services">
+      <div className="services__top">
+        {servicesItems.map((item, index) => {
+          return <div className="service" key={item.id} style={index === 1 ? {direction: 'rtl'} : null}>
+            <div className="service__left" style={index === 1 ? {alignItems: 'flex-end'} : null}>
+              <h1>{item.header}</h1>
+              <p style={index === 1 ? {direction: 'ltr'} : null}>{item.description}</p>
+              <Button buttonType='quinary' name='contact' />
+            </div>
+            <div className="service__right">
+              <img src={item.url} alt="mountain" />
+            </div>
+          </div>;
+        })}
+      </div>
+      <div className="services__accordion"></div>
+    </div>
+  );
 };
 
 export default Services;
