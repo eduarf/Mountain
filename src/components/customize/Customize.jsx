86d7717ebc9082bdcae6eb1134d1432a66237531
eduarf <fraude_f@yahoo.com>
2023-07-09 04:52:01 +0300

    about responsive

--- src/components/customize/Customize.jsx
+++ src/components/customize/Customize.jsx
@@ -18,7 +18,10 @@ const Customize = () => {
       </div>
       {/*  */}
       {/* Palet */}
-      <div className="default-container" onClick={() => dispatch(setTheme('default'))}>
+      <div
+        className="default-container"
+        onClick={() => dispatch(setTheme("default"))}
+      >
         <span>Default Colors</span>
         <div className="default-palet">
           <div></div>
@@ -44,19 +47,49 @@ const Customize = () => {
           secondary="rgb(255,81,0)"
           name="orange"
         />
-        <ColorPalet primary="rgb(48,199,181)" secondary="rgb(0,172,151)"  name='green' />
-        <ColorPalet primary="rgb(47,193,255)" secondary="rgb(8,172,242)" name='turquoise' />
-        <ColorPalet primary="rgb(65,117,252)" secondary="rgb(8,74,243)" name='blue' />
-        <ColorPalet primary="rgb(27,174,112)" secondary="rgb(6,117,46)" name='darkgreen' />
-        <ColorPalet primary="rgb(249,195,73)" secondary="rgb(255,177,0)" name='yellow' />
-        <ColorPalet primary="rgb(255,106,151)" secondary="rgb(250,3,107)" name='red' />
-        <ColorPalet primary="rgb(239,77,72)" secondary="rgb(217,7,0)" name='darkred' />
+        <ColorPalet
+          primary="rgb(48,199,181)"
+          secondary="rgb(0,172,151)"
+          name="green"
+        />
+        <ColorPalet
+          primary="rgb(47,193,255)"
+          secondary="rgb(8,172,242)"
+          name="turquoise"
+        />
+        <ColorPalet
+          primary="rgb(65,117,252)"
+          secondary="rgb(8,74,243)"
+          name="blue"
+        />
+        <ColorPalet
+          primary="rgb(27,174,112)"
+          secondary="rgb(6,117,46)"
+          name="darkgreen"
+        />
+        <ColorPalet
+          primary="rgb(249,195,73)"
+          secondary="rgb(255,177,0)"
+          name="yellow"
+        />
+        <ColorPalet
+          primary="rgb(255,106,151)"
+          secondary="rgb(250,3,107)"
+          name="red"
+        />
+        <ColorPalet
+          primary="rgb(239,77,72)"
+          secondary="rgb(217,7,0)"
+          name="darkred"
+        />
       </div>
       {/*  */}
-      <div className="icon-container" onClick={() => setIsOpen(!isOpen)}>
-        <BsFillPaletteFill className="icon" />
-        <span>Customize</span>
-      </div>
+      {!isOpen && (
+        <div className="icon-container" onClick={() => setIsOpen(!isOpen)}>
+          <BsFillPaletteFill className="icon" />
+          <span>Customize</span>
+        </div>
+      )}
     </div>
   );
 };
